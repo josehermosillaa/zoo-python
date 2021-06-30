53420b1878a0d94a8b24ad32b1bef532c19197e1
Jose Hermosilla <jose.hermosillaasach.cl>
2021-06-29 22:04:19 -0400

    se comienza la aplicacion, pero aun falta por terminar

--- zooInteractivo.py
+++ zooInteractivo.py
@@ -39,21 +39,45 @@ class Zoo:
             animal.alimentar()
         return self
     
-
+zoo = []
 while True:
+
     print("Bienvenido al sistema de ZOOlogicos")
-    menu = input("1.Agregar Zoologico \n2.Agregar Animal \n 3.Mostrar Zoologicos \n4.Alimentar Animales \n5.Salir \n Ingrese una opcion: ")
+    menu = input("1.Agregar Zoologico \n2.Ingresar a Zoologico \n3.Quitar Zoologicos \n4.Salir \n Ingrese una opcion: ")
     if menu == "1":
-        pass
+        nombre_zoologico = input("Ingrese el nombre del Zoologico:")
+        # zoo.append(nombre_zoologico)
+        zoologico= Zoo(f"{nombre_zoologico}")
+        zoo.append(zoologico)
+        print(f"se ha agregado el zoologico {zoologico.name}")
+        time.sleep(2)
+        continue
     elif menu == "2":
-        pass
+        print("los zoologicos agregados son:")
+        for zoologico in zoo:
+            print(zoo.index(zoologico),"-",">",zoologico.name)
+        while True:
+            i = int(input("ingrese la opcion de su zoologico:"))
+            print("zoologico seleccionado",zoo[i].name)
+            time.sleep(2)
+            seleccion=input("1. Agregar Animal \n2. Mostrar Animales \n3. Alimentar Animales \n4. Salir del Zoologico")
+            if seleccion == 1:
+                pass
+            elif seleccion == 2:
+                pass
+            elif seleccion == 3:
+                pass
+            elif seleccion == 4:
+                break
     elif menu == "3":
-        pass
+        continue
     elif menu == "4":
-        pass
-    elif menu == "5":
+        print("Hasta pronto")
+        time.sleep(1)
         break
     else:
         print("tecla no reconocida")
         time.sleep(1) #en segundos
         pass
+
+
